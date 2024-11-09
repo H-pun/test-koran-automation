74a5bd88add5bc6a7cd2d4d4820e2230b72017d0
Harvan Nurluthfi <harvan.nurluthfi@gmail.com>
2024-11-10 00:46:02 +0700

    Optimize code

--- main.py
+++ main.py
@@ -16,9 +16,8 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 
 
-def click_button(xpath: str):
-    button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, xpath)))
-    button.click()
+def click_element(xpath: str):
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, xpath))).click()
 
 
 try:
@@ -26,16 +25,14 @@ try:
 
     # Click through the initial setup
     for xpath in ["//button[text()='PLAY']", f"//label[@for='rd{TIME_LIMIT.value}']", "//label[@for='rdk1']", "//button[text()='OK']", "//button[text()='Mulai']"]:
-        click_button(xpath)
+        click_element(xpath)
         print(driver.find_element(By.XPATH, xpath).text)
 
     time.sleep(1)
 
     while driver.find_element(By.CLASS_NAME, "waktu").text != "00:00:00":
-        num1, num2 = [int(driver.find_element(By.CLASS_NAME, cls).text) for cls in ["soal1", "soal2"]]
-        result = (num1 + num2) % 10
-        body = driver.find_element(By.TAG_NAME, "body")
-        body.send_keys(result if random.random() > ERROR_RATE else (result + 1) % 10)
+        result = sum([int(driver.find_element(By.CLASS_NAME, cls).text) for cls in ["soal1", "soal2"]]) % 10
+        driver.find_element(By.TAG_NAME, "body").send_keys(result if random.random() > ERROR_RATE else (result + 1) % 10)
         time.sleep(DELAY)
 
 finally:
